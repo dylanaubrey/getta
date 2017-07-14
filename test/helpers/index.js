9fff556502532358a911a1326cd5123b73156848
Dylan Aubrey <dylan.aubrey@gmail.com>
2017-07-14 21:29:02 +0100

    Updating test method names.

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -198,7 +198,7 @@ export const productArgs = function productArgs(resource) {
 export const setupDelete = function setupDelete({ batch, newInstance = true, resource }) {
   const { urls } = mockDelete({ batch, resource });
   const getta = new Getta({ baseURL, cachemapOptions, newInstance });
-  getta.shortcut('delete', 'deleteProducts', { options: { batch }, path });
+  getta.shortcut('delete', 'deleteProduct', { options: { batch }, path });
   return { fetchMock, getta, urls };
 };
 
@@ -212,7 +212,7 @@ export const setupDelete = function setupDelete({ batch, newInstance = true, res
 export const setupDeleteAll = function setupDeleteAll({ newInstance = true, resource }) {
   const { url } = mockDeleteAll({ resource });
   const getta = new Getta({ baseURL, cachemapOptions, newInstance });
-  getta.shortcut('delete', 'deleteProducts', { path });
+  getta.shortcut('delete', 'deleteProduct', { path });
   return { fetchMock, getta, url };
 };
 
@@ -228,7 +228,7 @@ export const setupDeleteAll = function setupDeleteAll({ newInstance = true, reso
 export const setupGet = function setupGet({ batch, newInstance = true, queryParams, resource }) {
   const { urls } = mockGet({ batch, queryParams, resource });
   const getta = new Getta({ baseURL, cachemapOptions, newInstance });
-  getta.shortcut('get', 'getProducts', { options: { batch }, path, queryParams });
+  getta.shortcut('get', 'getProduct', { options: { batch }, path, queryParams });
   return { fetchMock, getta, urls };
 };
 
@@ -242,7 +242,7 @@ export const setupGet = function setupGet({ batch, newInstance = true, queryPara
 export const setupGetAll = function setupGetAll({ newInstance = true, resource }) {
   const { url } = mockGetAll({ resource });
   const getta = new Getta({ baseURL, cachemapOptions, newInstance });
-  getta.shortcut('get', 'getProducts', { path });
+  getta.shortcut('get', 'getProduct', { path });
   return { fetchMock, getta, url };
 };
 
@@ -256,7 +256,7 @@ export const setupGetAll = function setupGetAll({ newInstance = true, resource }
 export const setupPost = function setupPost({ newInstance = true, resource }) {
   const { url } = mockPost({ resource });
   const getta = new Getta({ baseURL, cachemapOptions, newInstance });
-  getta.shortcut('post', 'postProducts', { path });
+  getta.shortcut('post', 'postProduct', { path });
   return { fetchMock, getta, url };
 };
 
